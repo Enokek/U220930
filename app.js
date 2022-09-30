a1ff9921f690812d76775bf591bdb7bca29d9593
Enokek <enok.ekelund@gmail.com>
2022-09-30 14:44:39 +0200

    Kinda finished the project, unsure on how to make forEach

--- app.js
+++ app.js
@@ -4,21 +4,39 @@ const header = new XMLHttpRequest ();
         header.open('GET', 'structure.json')
         header.send();
 
-        header.onload = () => {
-        const keys = JSON.parse(header.responseText)
+const anka = new XMLHttpRequest ();
+          
 
-            
+        anka.open('GET', 'cars.json')
+        anka.send();
+
+
+LoadCars = () => {carsList.hidden = false};
+       
+header.onload = () => {
+       
+    const keys = JSON.parse(header.responseText)
 
         for(var key in keys){
             var header2 = document.createElement(key)
             header2.innerHTML = (keys[key]);
             
             Frame.appendChild(header2)
-           
-        
         }
-            
-
-
         };
+
+        
+anka.onload = () => {
+    var cars = JSON.parse(anka.responseText);
+    var list = document.createElement("ul");
+    carsList.appendChild(list);
+
+            for(var brand in cars) {
+                var listinUL = document.createElement("li");
+                listinUL.innerHTML = (cars[brand]);
+                carsList.appendChild(listinUL);
+            }
+            
+        }
+       
        
